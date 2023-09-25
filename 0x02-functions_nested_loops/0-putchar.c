2522e33969220f68560155a3595582bef6fa44e8
Grace Donkor <grace.donkor8@gmail.com>
2023-09-25 00:54:31 +0000

    Nested Loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Prints "_putchar" and followed by a new line.
+ *
+ * Return: Always 0.
+*/
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
